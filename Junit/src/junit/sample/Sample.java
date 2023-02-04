9356f300b4d7ac993d831f0ea3a088858612a343
yoshitaro-Yo2 <kichivs.mp@gmail.com>
2023-02-04 18:11:40 +0900

    first commit

--- Junit/src/junit/sample/Sample.java
+++
@@ -1,7 +0,0 @@
-package junit.sample;
-
-public class Sample {
-    public int add(int x, int y) {	  //足し算
-        return x + y;
-    }
-}
--- Junit/src/module-info.java
+++
@@ -1,6 +0,0 @@
-module Junit {
-	requires org.junit.jupiter.api;
-	requires org.junit.jupiter.params;
-	requires java.sql;
-//	requires dbunit;
-}
